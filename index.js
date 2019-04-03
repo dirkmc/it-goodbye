2a3c0e486295955f9676760df8ee278423c5333a
Dirk McCormick <dirkmdev@gmail.com>
2019-04-03 09:09:27 -0400

    fix: handle Buffers/strings and other types separately

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const endable = require('./endable')
 
+// Indicates if object can be converted to a Buffer
+const isBufferCompatible = (o) => Buffer.isBuffer(o) || typeof o === 'string'
+
 module.exports = (stream, goodbye) => {
-  goodbye = Buffer.from(goodbye || 'GOODBYE')
+  goodbye = goodbye || 'GOODBYE'
   const e = endable(goodbye)
+  const token = isBufferCompatible(goodbye) ? Buffer.from(goodbye) : goodbye
 
   return {
     // when the source ends,
@@ -12,16 +16,24 @@ module.exports = (stream, goodbye) => {
     sink: source => stream.sink((async function * () {
       // when the goodbye is received, allow the source to end.
       for await (const chunk of source) {
-        const buff = Buffer.from(chunk)
-        const done = buff.slice(-goodbye.length).equals(goodbye)
-        if (done) {
-          const remaining = buff.length - goodbye.length
-          if (remaining > 0) {
-            yield buff.slice(0, remaining)
+        if (isBufferCompatible(chunk)) {
+          const buff = Buffer.from(chunk)
+          const done = buff.slice(-token.length).equals(token)
+          if (done) {
+            const remaining = buff.length - token.length
+            if (remaining > 0) {
+              yield buff.slice(0, remaining)
+            }
+            e.end()
+          } else {
+            yield buff
           }
-          e.end()
         } else {
-          yield buff
+          if (chunk === goodbye) {
+            e.end()
+          } else {
+            yield chunk
+          }
         }
       }
     })())
